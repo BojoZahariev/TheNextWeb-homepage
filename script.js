f68c5d2a21f4e0dd713610fdcc4fbb4a0df4a6b0
Bojo <bozhidar_zahariev@hotmail.com>
2019-04-13 15:46:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-
+window.onresize = function() {
+    toggle();
+  };
+  
+  window.onscroll = function() {
+    myFunction();
+  };
+  
+  var nav = document.getElementById("navid");
+  var sticky = nav.offsetTop;
+  var logo = document.getElementById("logo2id");
+  var biglogo = document.getElementById("logoid");
+  
+  function myFunction() {
+    if (window.pageYOffset > sticky) {
+      biglogo.style.display = "none";
+      logo.style.display = "block";
+    } else if (window.pageYOffset < sticky && window.innerWidth > 1024) {
+      biglogo.style.display = "none";
+      logo.style.display = "none";
+  
+    } else if (window.pageYOffset < sticky && window.innerWidth < 1024) {
+      logo.style.display = "none";
+      biglogo.style.display = "block";
+    }
+  }
+  
+  function toggle() {
+    if (window.innerWidth > 1024) {
+      biglogo.style.display = "none";
+      logo.style.display = "none";
+    } else if (window.innerWidth < 1024) {
+      logo.style.display = "none";
+      biglogo.style.display = "block";
+    }
+  }
