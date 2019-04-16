795e5c295a79bacdd1a0f0212738ee23be383f16
Bojo <bozhidar_zahariev@hotmail.com>
2019-04-16 06:35:17 +0100

    nav logo handling

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
 window.onresize = function() {
-    toggle();
-  };
-  
-  window.onscroll = function() {
-    myFunction();
-  };
-  
-  var nav = document.getElementById("navid");
-  var sticky = nav.offsetTop;
-  var logo = document.getElementById("logo2id");
-  var biglogo = document.getElementById("logoid");
-  
-  function myFunction() {
-    if (window.pageYOffset > sticky) {
-      biglogo.style.display = "none";
-      logo.style.display = "block";
-    } else if (window.pageYOffset < sticky && window.innerWidth > 1024) {
-      biglogo.style.display = "none";
-      logo.style.display = "none";
-  
-    } else if (window.pageYOffset < sticky && window.innerWidth < 1024) {
-      logo.style.display = "none";
-      biglogo.style.display = "block";
-    }
+  toggle();
+};
+
+window.onscroll = function() {
+  myFunction();
+};
+
+var nav = document.getElementById('navid');
+var sticky = nav.offsetTop;
+var logo = document.getElementById('logo2id');
+var biglogo = document.getElementById('logoid');
+var mainnav = document.getElementById('mainnavid');
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    biglogo.style.display = 'none';
+    logo.style.display = 'block';
+    mainnav.style.paddingLeft = '120px';
+  } else if (window.pageYOffset < sticky && window.innerWidth > 1024) {
+    biglogo.style.display = 'none';
+    logo.style.display = 'none';
+    mainnav.style.paddingLeft = '15px';
+  } else if (window.pageYOffset < sticky && window.innerWidth < 1024) {
+    logo.style.display = 'none';
+    biglogo.style.display = 'block';
   }
-  
-  function toggle() {
-    if (window.innerWidth > 1024) {
-      biglogo.style.display = "none";
-      logo.style.display = "none";
-    } else if (window.innerWidth < 1024) {
-      logo.style.display = "none";
-      biglogo.style.display = "block";
-    }
+}
+
+function toggle() {
+  if (window.innerWidth > 1024) {
+    biglogo.style.display = 'none';
+    logo.style.display = 'none';
+    mainnav.style.paddingLeft = '15px';
+  } else if (window.innerWidth < 1024) {
+    logo.style.display = 'none';
+    biglogo.style.display = 'block';
+    mainnav.style.paddingLeft = '120px';
   }
+}
